542afa96740f7b9dfc85862afa18c594bb2d2c4b
Patryk <patryk.d.janus@gmail.com>
2024-08-02 18:40:06 +0200

    setup TS and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	modules: ['@nuxtjs/tailwindcss'],
+	tailwindcss: {
+		cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'first' }],
+		configPath: '~/tailwind.config.js',
+		exposeConfig: {
+			level: 2,
+		},
+		config: {},
+		viewer: true,
+	},
 })
