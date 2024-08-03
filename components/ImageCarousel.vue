7f2cfd7153d8f1851d7fa4ac5056f55fecba62a3
Patryk <patryk.d.janus@gmail.com>
2024-08-03 16:09:41 +0200

    add first section

---
+++ components/ImageCarousel.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="mt-[61px] w-full xl:w-[85%]">
+		<NuxtImg
+			class="w-full xl:w-full"
+			:src="imgSrc"
+		/>
+		<div class="flex justify-between mt-4">
+			<button @click="changeImageSrc('box2.png')">
+				<NuxtImg
+					class="w-[132px] h-[132px]"
+					src="box2.png"
+				/>
+			</button>
+			<button
+				class=""
+				@click="changeImageSrc('box3.png')"
+			>
+				<NuxtImg
+					class="w-[132px] h-[132px]"
+					src="box3.png"
+				/>
+			</button>
+			<button @click="changeImageSrc('box4.png')">
+				<NuxtImg
+					class="w-[132px] h-[132px]"
+					src="box4.png"
+				/>
+			</button>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const imgSrc = ref('box1.png')
+
+function changeImageSrc(src: string) {
+	imgSrc.value = src
+}
+</script>
---
+++ components/MysteryBoxForm.vue
@@ -0,0 +1,68 @@
+<template>
+	<h1 class="text-[52px] font-extrabold">
+		Chance to win a StockX Mystery Box today!
+	</h1>
+	<p class="font-normal text-[22px]">
+		Unleash the Excitement of Mystery Electronics
+	</p>
+	<form
+		action=""
+		class="rounded-[20px] border-solid border border-[#DBDBDB] p-6 mt-6"
+	>
+		<h2 class="text-[22px] font-medium flex justify-center align-middle">
+			Enter details
+		</h2>
+		<div class="grid grid-cols-12 gap-y-4 my-6">
+			<input
+				class="border rounded-[10px] border-[#808080] px-4 py-3 col-start-1 col-span-6 mr-4 focus:placeholder-black"
+				type="text"
+				placeholder="Last name"
+			/>
+			<input
+				class="border rounded-[10px] border-[#808080] px-4 py-3 col-start-7 col-span-6 focus:placeholder-black"
+				type="text"
+				placeholder="First name"
+			/>
+			<input
+				class="border rounded-[10px] border-[#808080] px-4 py-3 col-span-full focus:placeholder-black"
+				type="text"
+				placeholder="Streat"
+			/>
+			<input
+				class="border rounded-[10px] border-[#808080] px-4 py-3 col-start-1 col-span-6 mr-4 focus:placeholder-black"
+				type="text"
+				placeholder="Postal code"
+			/>
+			<input
+				class="border rounded-[10px] border-[#808080] px-4 py-3 col-start-7 col-span-6 focus:placeholder-black"
+				type="text"
+				placeholder="City"
+			/>
+			<input
+				class="border rounded-[10px] border-[#808080] px-4 py-3 col-span-full focus:placeholder-black"
+				type="tel"
+				placeholder="Phone number"
+			/>
+			<input
+				class="border rounded-[10px] border-[#808080] px-4 py-3 col-span-full focus:placeholder-black"
+				type="email"
+				placeholder="E-mail"
+			/>
+		</div>
+		<button
+			class="bg-[#006340] text-white py-3 w-full rounded-[10px] mb-6 font-semibold text-base"
+			type="submit"
+		>
+			Proceed to payment
+		</button>
+        <div class="flex justify-center">
+            <NuxtImg class="mr-4" src='McAfee.png'/>
+            <NuxtImg class="mr-4" src='TRUSTe.png'/>
+            <NuxtImg src='256BIT.png'/>
+        </div>
+	</form>
+</template>
+
+<script>
+export default {}
+</script>
