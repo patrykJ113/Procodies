a7ad9a52de5e4c2953a1ad1a7248cc531587a90d
Patryk <patryk.d.janus@gmail.com>
2024-08-03 15:35:39 +0200

    add nuxt/image in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
+	components: true,
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss'],
+	modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
 	tailwindcss: {
-		cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'first' }],
+		cssPath: '~/assets/css/tailwind.css',
 		configPath: '~/tailwind.config.js',
 		exposeConfig: {
 			level: 2,
@@ -12,4 +13,5 @@ export default defineNuxtConfig({
 		config: {},
 		viewer: true,
 	},
+	image: {},
 })
